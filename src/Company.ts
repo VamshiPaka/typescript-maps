3a872aeb7d83691ecf53776cd9e928bd79f59066
VamshiPaka <vamshi.paka@gmail.com>
2021-03-21 19:06:17 +0530

    added label to the marker on map

--- src/Company.ts
+++ src/Company.ts
@@ -14,4 +14,10 @@ export class Company{
       lat:parseFloat(faker.address.longitude())
     }
   }
+  markerContent():string{
+    return `<div>
+            <h1>Company Name:${this.companyName}</h1>
+            <h3>Catchphrase:${this.catchPhrase}</h3>
+            </div>`
+  }
 }
