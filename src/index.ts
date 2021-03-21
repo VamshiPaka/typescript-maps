141a525e464898202b12e241e278e26f78af93f1
VamshiPaka <vamshi.paka@gmail.com>
2021-03-21 14:36:58 +0530

    restricting access with interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import {User} from './User';
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 const user = new User();
-console.log(user);
+
 const company = new Company();
-console.log(company);
-new google.maps.Map(document.getElementById("map"),{zoom:1,center:{
-  lat:0,
-  lng:0
-}});
+
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
+
+
+
+ 
